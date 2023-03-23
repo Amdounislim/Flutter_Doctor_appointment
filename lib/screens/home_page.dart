7f3dfc73de05aa755d5ca0dc39e71901ee67fadf
Hachem Alouane <hachem.alouane@devnet.me>
2023-03-23 13:24:40 +0100

    Build_Home_Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,9 +11,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    Config().init(context);
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Slim",
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assets/profile1.jpg'),
+                  ),
+                )
+              ],
+            ),
+            Config.spaceSmall,
+          ],
+        )),
+      ),
     );
-    ;
   }
 }
+
+
+//  Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children:const <Widget>[
+//                  Text(
+//               "Slim",
+//               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//               ]),
