971a17c22ede76f90f26441d3762babb14a88f0c
Hachem Alouane <hachem.alouane@devnet.me>
2023-03-24 09:48:23 +0100

    Doctor_Card_Widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_appointment/components/appointment_card.dart';
+import 'package:doctor_appointment/components/doctor_card.dart';
 import 'package:doctor_appointment/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -45,80 +46,88 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         child: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Slim",
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/profile1.jpg'),
+            child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Slim",
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            ),
-            Config.spaceMedium,
-            Text(
-              "Category",
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Config.spaceSmall,
-            SizedBox(
-              height: Config.heightSize * 0.05,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: medCat.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    margin: const EdgeInsets.only(right: 20),
-                    color: Config.primaryColor,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          FaIcon(
-                            medCat[index]['icon'],
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          Text(
-                            medCat[index]['category'],
-                            style: const TextStyle(
-                                fontSize: 16, color: Colors.white),
-                          ),
-                        ],
-                      ),
+                  SizedBox(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/profile1.jpg'),
                     ),
-                  );
-                },
+                  )
+                ],
               ),
-            ),
-            Config.spaceSmall,
-            const Text(
-              'Appointment Today',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Config.spaceSmall,
-            AppointmentCard(),
-            Config.spaceSmall,
-            const Text(
-              'Top Doctors',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            
-          ],
+              Config.spaceMedium,
+              Text(
+                "Category",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Config.spaceSmall,
+              SizedBox(
+                height: Config.heightSize * 0.05,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: medCat.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      margin: const EdgeInsets.only(right: 20),
+                      color: Config.primaryColor,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            FaIcon(
+                              medCat[index]['icon'],
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              medCat[index]['category'],
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Config.spaceSmall,
+              const Text(
+                'Appointment Today',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Config.spaceSmall,
+              AppointmentCard(),
+              Config.spaceSmall,
+              const Text(
+                'Top Doctors',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Config.spaceSmall,
+              Column(
+                children: List.generate(10, (index) {
+                  return DoctorCard();
+                }),
+              )
+            ],
+          ),
         )),
       ),
     );
